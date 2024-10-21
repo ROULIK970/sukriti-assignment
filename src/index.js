103a0d5b62a4e0d76de7b1944233c76d95fc31a6
Pritish <bordoloipritish97@gmail.com>
2024-10-21 19:10:49 +0530

    made util classes and hof

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
+import { app } from "./app.js";
 
 dotenv.config({
     path:"./env"
 })
 
 connectDB()
+.then(() =>{
+    app.listen(process.env.PORT || 3000, () =>{
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((e) =>{
+    console.log("MongoDB connection error")
+})
