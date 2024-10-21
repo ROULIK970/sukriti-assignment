253729143fdb768fe9e3ec9b64a67086afbbd7bd
Pritish <bordoloipritish97@gmail.com>
2024-10-21 20:21:44 +0530

    added controllers and token handlers

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-const app = express()
-app.use(cors({
-    origin: process.env.CORS_ORIGIN
-}))
+const app = express();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-app.use(express.json())
-app.use(express.urlencoded)
-app.use(cookieParser())
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
-export {app}
+//routes
+
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
+
+export { app };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,9 +2,9 @@ const asyncHandler = (fn) => async(req,res, next) =>{
  try {
     await fn(req,res,next)
  } catch (e) {
-    res.status(err.code || 500).json({
+    res.status(e.code || 500).json({
         success: false,
-        message: err.message
+        message: e.message
     })
  }
 }
